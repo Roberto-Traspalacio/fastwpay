d35bc69bdf86e723d87b8e4c07e1591456bd988b
soyrbto <soyrbto@gmail.com>
2022-12-27 10:15:15 -0400

    update

--- components/Contact.js
+++ components/Contact.js
@@ -10,9 +10,7 @@ export default function contact({ className = "" }) {
 			className={`${className} sm:flex sm:justify-evenly grid-main gap-x-3 justify-items-center pt-9 gap-y-8 bg-background-2 full-bleed-2`}
 		>
 			<div className='typo-body-1 col-span-2 flex  flex-col items-center'>
-				<div className='relative h-[18px] w-[18px] xsm:h-6 xsm:w-6 inline-block mb-2'>
-					<Image src={phone} alt='Phone icon' fill={true} />
-				</div>
+				<Image className='mb-2' src={phone} alt='Phone icon' />
 				<div className='font-bold leading-[165%] text-color-2 text-text-2'>
 					Phone number
 				</div>
@@ -21,9 +19,7 @@ export default function contact({ className = "" }) {
 				</div>
 			</div>
 			<div className='typo-body-1 col-span-2 col-start-2 flex  flex-col items-center'>
-				<div className='relative h-[18px] w-[18px] xsm:h-6 xsm:w-6 inline-block mb-2'>
-					<Image src={location} alt='Phone icon' fill={true} />
-				</div>
+				<Image className='mb-2' src={location} alt='Phone icon' />
 				<div className='font-bold leading-[165%] text-text-2 text-center'>
 					Address
 				</div>
@@ -32,9 +28,7 @@ export default function contact({ className = "" }) {
 				</div>
 			</div>
 			<div className='typo-body-1 flex flex-col col-span-2 col-start-3 row-start-1 sm:flex  sm:flex-col items-center sm:w-[150px] '>
-				<div className='relative h-[18px] w-[18px] xsm:h-6 xsm:w-6 inline-block mb-2'>
-					<Image src={email} alt='Phone icon' fill={true} />
-				</div>
+				<Image className='mb-2' src={email} alt='Phone icon' />
 				<div className='font-bold leading-[165%] text-text-2 text-center'>
 					Email
 				</div>
