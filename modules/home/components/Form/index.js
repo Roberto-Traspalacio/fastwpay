6e1a2a6df294ed0e0892a3f9e4ab5f95ee2a4ab8
victormanuelrequena <requenadev1@gmail.com>
2023-02-24 12:12:43 -0400

    update

--- modules/home/components/Form/index.js
+++ modules/home/components/Form/index.js
@@ -31,7 +31,7 @@ export default function Form({ children }) {
   return (
     <form
       name="contact"
-      action="/"
+      action="/success"
       method="POST"
       data-netlify="true"
       className="grid-main bg-background-2 pt-14 gap-x-3 full-bleed-section lg:pb-20 xl:items-start"
