b6ac429e9301baa87d74709cfceaa181674c721d
Roberto <soyrbto@gmail.com>
2023-07-26 08:37:14 -0400

    pushing a test for vercel

--- components/BannerBlue.js
+++ components/BannerBlue.js
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 import IntlMessages from 'utils/IntlMessages';
-import { DOWNLOAD_PLUGIN_URL } from 'utils/common';
 
 export default function BannerBlue({ className }) {
   return (
     <div
-      className={`grid-main gap-x-3 col-span-full bg-background-2 py-[16px] mt-[10px] sm:mt-[32px] sm:rounded-2xl sm:min-h-[106px] md:flex md:items-center md:justify-center md:gap-[38px] md:py-4 md:h-auto md:min-h-0 lg:col-span-full ${className}`}
+      className={`grid-main gap-x-3 col-span-full bg-background-2 py-[16px] h-[110px] mt-[10px] sm:mt-[32px] sm:rounded-2xl sm:min-h-[106px] md:flex md:items-center md:justify-center md:gap-[38px] md:py-4 md:h-auto md:min-h-0 lg:col-span-full ${className}`}
     >
       <h3 className="typo-body-1 text-center m-auto max-w-[32ch] col-span-full text-text-4 sm:max-w-none md:m-0">
         <IntlMessages id="dashboard.bannerBlue.text" />
       </h3>
-      <Link className="col-span-full" href={DOWNLOAD_PLUGIN_URL}>
+      <Link
+        className="col-span-full"
+        href="https://drive.google.com/uc?id=1yI-okBcvmz-cQMVOZVTP0Bb-3dqWyCND&export=download"
+      >
         <button className="m-auto flex items-center justify-center bg-primary-blue text-white py-[10px] px-[24px] h-[34px] rounded-full mt-3 sm:mt-[14px] sm:min-h-[38px] md:m-0">
           <p className="typo-body-1">
             <IntlMessages id="common.download" />
